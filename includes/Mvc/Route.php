188407d8d99388b3a1ca1ae64157eb8899e335c1
Your Name <you@example.com>
2014-11-03 23:10:59 +0200

    add Twig

--- includes/Mvc/Route.php
+++ includes/Mvc/Route.php
@@ -3,6 +3,8 @@
 namespace Mvc;
 
 use Mvc\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
@@ -24,10 +26,17 @@ class Route
         // routes
         $routes->add('page', new \Symfony\Component\Routing\Route('/page/{name}'));
         $routes->add('home', new \Symfony\Component\Routing\Route('/'));
-        $routes->add('home1', new \Symfony\Component\Routing\Route('/{name}'),array('name' => 'home'));
+        $routes->add('home1', new \Symfony\Component\Routing\Route('/{name}'), array('name' => 'home'));
 
         $context->fromRequest($request);
-        $attributes = $matcher->match($request->getPathInfo());
+
+        try {
+            $attributes = $matcher->match($request->getPathInfo());
+        } catch (ResourceNotFoundException $e) {
+            $response = new Response('Not found!', Response::HTTP_NOT_FOUND);
+            return $response->send();
+        }
+
         $route = $attributes['_route'];
 
         if ($route == 'home' || $route == 'home1') {
--- includes/Mvc/Views/View.php
+++ includes/Mvc/Views/View.php
@@ -2,6 +2,8 @@
 
 namespace Mvc\Views;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Class View
  * @package Mvc\Views
@@ -14,6 +16,23 @@ class View
      */
     public function render($html, $data = '')
     {
-        include sprintf('templates/%s.php', $html);
+        \Twig_Autoloader::register();
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/templates');
+        $twig = new \Twig_Environment($loader, array(
+            'cache' => 'compilation_cache',
+            'debug' => true
+        ));
+        $twig->addExtension(new \Twig_Extension_Debug());
+
+        try {
+            $template = $twig->loadTemplate(sprintf('%s.html.twig', $html));
+        } catch (\Twig_Error_Loader $e) {
+            $response = new Response('Not found!', Response::HTTP_NOT_FOUND);
+            return $response->send();
+        }
+
+        $renderTpl = $template->render(array('items' => $data));
+        $response = new Response($renderTpl);
+        $response->send();
     }
-} 
+}
